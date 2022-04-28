8069cc90f9067cdc4423b4510d7326816d6de933
Leonardo Melati <leomelati@gmail.com>
2022-04-28 01:29:03 -0300

    Remove logo

---
+++ src/models/Destination.ts
@@ -0,0 +1,9 @@
+/* eslint-disable require-jsdoc */
+export class Destination {
+  Id!: string;
+  Name!: string;
+  Description!: string;
+  Image!: string;
+  Distance!: number;
+  TravelTime!: number;
+}
